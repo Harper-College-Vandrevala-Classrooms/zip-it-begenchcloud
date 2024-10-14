6efef68500be38c973d1c18cfe90f9ee2982f4ea
Ben <b_abdyvaidov@mail.harpercollege.edu>
2024-10-14 12:40:38 -0500

    finished

--- src/main/java/com/csc/Zipper.java
+++ src/main/java/com/csc/Zipper.java
@@ -1,5 +1,22 @@
 package com.csc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zipper {
 
+    public static <T> List<T> zip(List<T> list1, List<T> list2) {
+        List<T> mergedList = new ArrayList<>();
+        int maxLength = Math.max(list1.size(), list2.size());
+
+        for (int i = 0; i < maxLength; i++) {
+            if (i < list1.size()) {
+                mergedList.add(list1.get(i));
+            }
+            if (i < list2.size()) {
+                mergedList.add(list2.get(i));
+            }
+        }
+        return mergedList;
+    }
 }
--- src/test/java/com/csc/ZipperTest.java
+++ src/test/java/com/csc/ZipperTest.java
@@ -1,21 +1,43 @@
 package com.csc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ZipperTest {
 
-  Zipper zipper;
+Zipper zipper;
 
-  @BeforeEach
-  void setUp() {
+@BeforeEach
+void setUp() {
     zipper = new Zipper();
-  }
+}
 
-  @Test
-  void itWorks() {
-    assertEquals(true, true);
-  }
+@Test
+void testZipWithEqualSizeLists() {
+    List<Integer> nums1 = List.of(1, 3, 5, 7);
+    List<Integer> nums2 = List.of(2, 4, 6, 8);
+    List<Integer> result = Zipper.zip(nums1, nums2);
+    assertEquals(List.of(1, 2, 3, 4, 5, 6, 7, 8), result);
+}
+
+@Test
+void testZipWithUnequalSizeLists() {
+    List<String> colors1 = List.of("Red", "Green", "Blue");
+    List<String> colors2 = List.of("White", "Black", "Orange", "Pink");
+    List<String> result = Zipper.zip(colors1, colors2);
+    assertEquals(List.of("Red", "White", "Green", "Black", "Blue", "Orange", "Pink"), result);
+}
+
+@Test
+void testZipWithEmptyList() {
+    List<String> colors1 = List.of();
+    List<String> colors2 = List.of("White", "Black", "Orange");
+    List<String> result = Zipper.zip(colors1, colors2);
+    assertEquals(List.of("White", "Black", "Orange"), result);
+}
+
+
 }
